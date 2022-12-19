a7f26f89f7e30575dc5a2faf44b0f165313195b0
russian-travel <88851629+komlyanovna@users.noreply.github.com>
2022-12-19 22:58:24 +0500

    Delete src directory

--- src/components/Search/Search.jsx
+++
@@ -1,9 +0,0 @@
-import SeachStyle from './style.module.css'
-
-export function Search() {
-  return (
-    <div className={SeachStyle.seach}>
-      <input placeholder="Поиск" />
-    </div>
-  )
-}
